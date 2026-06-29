26dd5850901371437a1e9ad44951517ee7cb300f
arghya-online <arghyaonline555@gmail.com>
2026-06-29 09:45:39 +0530

    tanstack query, shadcn setup

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
 };
 
 function createPrismaClient() {
